54ee5794c27e1d3d6e77dd9ca3cabd0cf3abe787
leoarmelin <leonardo99.armelin@gmail.com>
2022-05-30 14:13:22 -0300

    feat(theme): add colors for theme

---
+++ app/src/main/java/com/leoarmelin/learningchinese/ui/ThemeColors.kt
@@ -0,0 +1,10 @@
+package com.leoarmelin.learningchinese.ui
+
+import androidx.compose.ui.graphics.Color
+
+object ThemeColors {
+    val Background = Color(0xFF282A36)
+    val CurrentLine = Color(0XFF44475A)
+    val Foreground = Color(0XFFF8F8F2)
+    val Comment = Color(0XFF6272A4)
+}
